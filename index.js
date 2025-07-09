665f8343634974ebab8bcf0fd959649292b219ad
Kumarsai Makthala <kumarsai05072005@gmail.com>
2025-07-09 10:44:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+/*let userName = prompt("Enter your name to see my website:");
+console.log("Hello, " + userName);
+
+//alert user for output.
+alert("Hello, " + userName+" welcome! to my website.");
